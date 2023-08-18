2fcda7ebdd156ae0d71df32c3a6927bb17071813
janaVukHana <ilija009@gmail.com>
2023-08-18 17:11:15 +0200

    change transition on hero image

--- src/components/ImageHero.jsx
+++ src/components/ImageHero.jsx
@@ -5,7 +5,7 @@ import firemanImage from '../assets/firedep/fireman.png';
 
 const Img = styled.img`
     opacity: 0;
-    transition: 1s cubic-bezier(.07,.34,.94,.1);
+    transition: 0.5s cubic-bezier(.04,.95,1,.53);
 `
 
 const ImageHero = () => {
